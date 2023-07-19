6a53d35bcb1ccfbb0b0219c83cefaabfab37a931
Jonathan Wilson <Jonathan@Crystals-MacBook-Air.local>
2023-07-19 12:57:28 -0400

    added further thinking

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 import surveys
@@ -9,29 +9,45 @@ app.config['SECRET_KEY']="fenway2023"
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 
-responses = []
-questionNum=0
-a= surveys.satisfaction_survey
+
+#questionNum=0
+
 
 
 @app.route('/')
 def home_page():
   
-   return render_template('start.html', a = a)
+   return render_template('start.html')
+
+@app.route('/init', methods=['POST'])
+def init():
+    global a
+    global resp
+
+    resp = request.form.get('survey')
+    if resp == 'satisfaction_survey':
+        a = surveys.satisfaction_survey
+    else:
+        a = surveys.personality_quiz
+    
+    if session.get('personalityCompleted', False) and a == surveys.personality_quiz:
+        return redirect('/thankYouPC')
+    
+    if session.get('surveyCompleted', False) and a == surveys.satisfaction_survey:
+        return redirect('/thankYouPC')
+    
+    session['responses'+resp]=[]
+    
+    return redirect('question/0')
    
     
 @app.route('/question/<q>' )
 def question(q):
-    global responses
     qInt = int(q)
-    qsAnswered = len(responses)
-    if qInt == 4:
-        
-        responses=[]
-        return redirect('/question/0')
-    if qInt != qsAnswered:
-        flash(f"""Please answer the questions in order! You have been redirected to question {qsAnswered}!""")
-        return redirect("/question/"+str(qsAnswered))
+    
+    if qInt != len(session['responses'+resp]):
+        flash(f"""Please answer the questions in order! You have been redirected to question {len(session['responses'])}!""")
+        return redirect("/question/"+str(len(session['responses'+resp])))
     questionNo = qInt
     qInt += 1
     choices = a.questions[questionNo].choices
@@ -42,7 +58,10 @@ def question(q):
 @app.route('/answer/<q>', methods=["POST"])
 def answer(q):
     answer = request.form["choice"]
+    responses = session['responses'+resp]
     responses.append(answer)
+
+    session['responses'+resp]= responses
     
     if len(a.questions) != len(responses):
         return redirect('/question/'+q)
@@ -55,7 +74,17 @@ def answer(q):
 @app.route('/thankYou' )
 def thankYou():
     
-    
-    print(responses)
+    if a == surveys.personality_quiz:
+        session['personalityCompleted']=True
+    else:
+        session['surveyCompleted']=True
+        
+    print(session['responses'+resp])
    
-    return render_template('thankYou.html', responses = responses, a=a)
+    return render_template('thankYou.html', responses = session['responses'+resp], a=a)
+
+
+@app.route('/thankYouPC')
+def thankYouPC():
+    asdf = 'surveys.'+resp+'.title'
+    return render_template('thankYouPC.html', resp=resp)
